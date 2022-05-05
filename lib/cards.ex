14b9c1bb94484b8538b53ea6bb3b4067a120f454
Carlos Primo <carlosprimob@gmail.com>
2022-05-05 11:10:31 -0500

    I added a function to check if a card is in the deck

--- lib/cards.ex
+++ lib/cards.ex
@@ -2,7 +2,10 @@ defmodule Cards do
     def create_deck do
       ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
     end
-    def shuffle_deck(deck) do
+    def shuffle(deck) do
       Enum.shuffle(deck)
+    end
+    def contains(deck, card) do
+      Enum.member?(deck,card)
     end
 end
