1feef9e7dc377385e6190dd7a8eaa648c62424bb
Carlos Primo <carlosprimob@gmail.com>
2022-05-10 18:11:00 -0500

    login and logout completed

--- discuss/lib/discuss_web/controllers/topic_controller.ex
+++ discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -5,6 +5,9 @@ defmodule DiscussWeb.TopicController do
   alias Discuss.Topic
   alias Discuss.Repo
 
+  plug DiscussWeb.Plugs.RequireAuth when action in [:new, :create,:edit ,:update, :delete]
+  plug :check_topic_owner when action in [:edit, :update, :delete]
+
   def index(conn,_params) do
     topics = Repo.all(Topic)
     render conn, "index.html", topics: topics
@@ -16,7 +19,9 @@ defmodule DiscussWeb.TopicController do
   end
 
   def create(conn, %{"topic" => topic}) do
-    changeset = Topic.changeset(%Topic{}, topic)
+    changeset = conn.assigns.user
+    |> Ecto.build_assoc(:topics)
+    |> Topic.changeset(topic)
 
     case Repo.insert(changeset) do
       {:ok, _topic} ->
@@ -56,5 +61,19 @@ defmodule DiscussWeb.TopicController do
     conn
     |> put_flash(:info, "Topic deleted")
     |> redirect(to: Routes.topic_path(conn, :index))
+  end
+
+  def check_topic_owner(conn, _params) do
+    %{params: %{"id" => topic_id}} = conn
+
+    if Repo.get(Topic,topic_id).user_id == conn.assigns.user.id do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You can't edit other people's topics")
+      |> redirect(to: Routes.topic_path(conn, :index))
+      |> halt()
+    end
+
   end
 end
